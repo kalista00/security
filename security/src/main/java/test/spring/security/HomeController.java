7d14bf14004770d7cb7c7abacc048710cbb99804
김창희 <122184547+kalista00@users.noreply.github.com>
2023-09-30 23:36:27 +0900

    Update HomeController.java

--- security/src/main/java/test/spring/security/HomeController.java
+++ security/src/main/java/test/spring/security/HomeController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class HomeController {
-	
+	//시작로그
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	/**
